ee67bf6a8c02be35d46de3d6da5899a6b82e839b
Talia Frendl <taliafrendl@gmail.com>
2025-07-25 11:49:52 -0700

    start time and end time cannot be null

--- src/server/sql/conversionSegment/create_conversion_segments_table.sql
+++ src/server/sql/conversionSegment/create_conversion_segments_table.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS conversion_segments (
 	week_patterns_id INTEGER REFERENCES week_patterns(id),
 	slope FLOAT,
 	intercept FLOAT,
-	start_time TIMESTAMP DEFAULT '-infinity',
-	end_time TIMESTAMP DEFAULT 'infinity',
+	start_time TIMESTAMP NOT NULL DEFAULT '-infinity',
+	end_time TIMESTAMP NOT NULL DEFAULT 'infinity',
 	note TEXT,
 	FOREIGN KEY (source_id, destination_id) REFERENCES conversions(source_id, destination_id),
 	PRIMARY KEY (source_id, destination_id, start_time)
