c5ecc504507fee6c401c32857a0054191c67e399
PRINCESANCHEZ <princesanchez72@gmail.com>
2025-09-11 20:44:41 -0700

    remove unused exports
    Signed-off-by: PRINCESANCHEZ <princesanchez72@gmail.com>

--- src/client/app/redux/api/conversionSegmentsApi.ts
+++ src/client/app/redux/api/conversionSegmentsApi.ts
@@ -12,10 +12,6 @@ import { ConversionSegmentData, SplitConversionSegmentPayload } from '../../type
  */
 export const conversionSegmentsApi = baseApi.injectEndpoints({
 	endpoints: builder => ({
-		getConversionSegments: builder.query<ConversionSegmentData[], void>({
-			query: () => '/api/conversionSegments',
-			providesTags: ['ConversionSegments']
-		}),
 		getConversionSegmentByConversion: builder.query<ConversionSegmentData[], {sourceId: number; destinationId: number }>({
 			query: ({ sourceId, destinationId }) => ({
 				url: '/api/conversionSegments/sourceDestination',
@@ -26,17 +22,6 @@ export const conversionSegmentsApi = baseApi.injectEndpoints({
 				{ type: 'ConversionSegments', id: 'LIST' },
 				{ type: 'ConversionSegments', id: `${segment.sourceId}-${segment.destinationId}` }
 			]
-		}),
-		getConversionSegmentByKey: builder.query<ConversionSegmentData, { sourceId: number; destinationId: number; startTime: string; endTime: string }>({
-			query: ({ sourceId, destinationId, startTime, endTime }) => ({
-				url: '/api/conversionSegments/sourceDestinationStartEnd',
-				method: 'POST',
-				body: { sourceId, destinationId, startTime, endTime }
-			}),
-			providesTags: (result, error, segment) => [{
-				type: 'ConversionSegments',
-				id: `${segment.sourceId}-${segment.destinationId}-${segment.startTime}-${segment.endTime}`
-			}]
 		}),
 		editConversionSegment: builder.mutation<void, { segment: ConversionSegmentData; originalStartTime: string; originalEndTime: string; }>({
 			query: ({ segment, originalStartTime, originalEndTime }) => ({
@@ -126,9 +111,7 @@ export const conversionSegmentsApi = baseApi.injectEndpoints({
 });
 
 export const {
-	useGetConversionSegmentsQuery,
 	useGetConversionSegmentByConversionQuery,
-	useGetConversionSegmentByKeyQuery,
 	useEditConversionSegmentMutation,
 	useAddConversionSegmentMutation,
 	useSplitConversionSegmentEarlierMutation,
